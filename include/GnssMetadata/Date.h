efdcc89e4951a11d1a68bfd37bd180753c7a9187
Michael B Mathews <michael.mathews@loctronix.com>
2014-12-30 21:14:14 -0800

    Implemented functional Date class with support for GPS time conversion.

--- include/GnssMetadata/Date.h
+++ include/GnssMetadata/Date.h
@@ -19,27 +19,79 @@
 #ifndef DATE_H_H
 #define DATE_H_H
 
+//Grab os time type.
+#if defined(_WIN32) || defined(_WIN64)
+	#include <time.h>
+	struct timespec 
+	{
+		time_t tv_sec;
+		long tv_nsec;
+	};
+#else
+	#include <time.h>
+#endif
+
+
 #include "BaseTypes.h"
 namespace GnssMetadata
 {
+	/**
+	 * Simple Date and Time class with GPS time conversion support.
+	 */
 	class Date: public Object
 	{
-		
 	public:
-		Date() {}
-		Date( const char* pszDate) : _sdate( pszDate){}
-		Date( int wk, double seconds) 
-		{
+		static Date Now();
+		Date();
+		Date( const Date& rhs);
+		Date( const timespec& tspec);
+		Date(int year, int mon, int day, int hour, int min, double sec);
+		Date(time_t time, double fracsec = 0.0);
+		Date( double secGps, int wkGps, int secLeap = 0);
+		Date( const char* pszDate);
 
-		}
-		const Date& operator=( const Date& rhs){return *this;}
+		const Date& operator=( const Date& rhs);
+
+		const struct tm* TmPtr() const {return gmtime(&_dt.tv_sec);}
+		int Year() const 		{ int yr = gmtime(&_dt.tv_sec)->tm_year; yr+=1900; return yr;}
+		int Month() const 		{ return gmtime(&_dt.tv_sec)->tm_mon + 1; }
+		int Day() const			{ return gmtime(&_dt.tv_sec)->tm_mday; }
+		int DayOfYear() const	{ return gmtime(&_dt.tv_sec)->tm_yday; }
+		int DayOfWeek() const	{ return gmtime(&_dt.tv_sec)->tm_wday; }
+		int Hour() const		{ return gmtime(&_dt.tv_sec)->tm_hour; }
+		int Minute() const		{ return gmtime(&_dt.tv_sec)->tm_min;  }
+		double Second() const	{ return gmtime(&_dt.tv_sec)->tm_sec;  }
+
+		/**
+		 * Seconds since GPS 0
+		 */
+		timespec SinceGps0() const; 
+
+		/**
+		 * Returns the GPS week and seconds 
+		 */
+		void ToGpsWeekSec( int *pwkGps, double *psecGps) const;		
+
+		/**
+		 * Returns the GPS Week.  Note use ToGpsWeekSec for most efficient access.
+		 */
+		int  GpsWeek()   const { int wk; double sec; ToGpsWeekSec(&wk, &sec); return wk;}
+		
+		/**
+		 * Returns the GPS Sec.  Note use ToGpsWeekSec for most efficient access.
+		 */
+		double GpsSec() const { int wk; double sec; ToGpsWeekSec(&wk, &sec); return sec;}
+
 		/**
 		 *  Returns a string representation of the object.
 		 *  
 		 */
 		virtual String toString( const String & sFormat = DefaultFormat );
+
+
 	private:
-		String _sdate;
+		timespec _dt;
+		int _secLeap;
 	};
 	
 }
--- lib/GnssMetadata/Date.cpp
+++ lib/GnssMetadata/Date.cpp
@@ -17,13 +17,162 @@
  */
 
 #include <GnssMetadata/Date.h>
+#include <stdio.h>
+
+#if defined(_WIN32) || defined(_WIN64)
+	#include <Windows.h>
+#else
+	#define _snprintf snprintf
+#endif
+
+
 using namespace GnssMetadata;
 
+//time_t value for GPS0 in Windows.
+#define GPS0 315964800
+#define SECONDS_IN_WEEK (86400*7)
+#define BILLION_VALUE (1000000000)
+/******************************************************************************
+* timespec utilities
+******************************************************************************/
+
+static time_t UtcYmdhmsToTime_T(int year, int mon, int day, int hour, int min, int sec)
+{   struct tm *temp_tm;
+	time_t temp_time_t;
+	time(&temp_time_t);
+	temp_tm = gmtime(&temp_time_t);
+	temp_tm->tm_sec = sec;
+	temp_tm->tm_min = min;
+	temp_tm->tm_hour = hour;
+	temp_tm->tm_mday = day;
+	temp_tm->tm_mon = mon;
+	temp_tm->tm_year = (year > 1900 ? year - 1900 : year);
+	temp_time_t=mktime(temp_tm);    
+#if !defined  LINUX
+	temp_time_t-=_timezone;	// undo any correction for timezone/daylight savings system did
+#endif
+	return temp_time_t;
+}
+
+/******************************************************************************
+* Date class implementation
+******************************************************************************/
+
+Date Date::Now()
+{
+	Date date;
+#if defined(_WIN32) || defined(_WIN64)
+	FILETIME ft;
+	GetSystemTimeAsFileTime(&ft);
+
+	ULARGE_INTEGER ull;    
+	ull.LowPart = ft.dwLowDateTime;    
+	ull.HighPart = ft.dwHighDateTime;    
+	ull.QuadPart -= 116444736000000000ULL; // Since Jan 1 1970.
+	time_t t = (time_t) (ull.QuadPart / 10000000ULL);
+	ull.QuadPart -= t*10000000ULL; //Fractional 100 nano seconds.
+	date._dt.tv_sec  = t;
+	date._dt.tv_nsec = (ull.LowPart)*100;
+#elif defined(LINUX)
+	clock_gettime( CLOCK_REALTIME, &(date._dt));
+#endif
+	return date;
+}
+
+Date::Date() : _secLeap(0)
+{
+	_dt.tv_nsec = 0;
+	_dt.tv_sec = 0;
+}
+
+Date::Date( const Date& rhs)
+: _dt(rhs._dt), _secLeap( rhs._secLeap)
+{
+
+}
+Date::Date( const timespec& tspec)
+: _dt( tspec), _secLeap(0)
+{
+}
+Date::Date(int year, int mon, int day, int hour, int min, double sec)
+: _secLeap(0)
+{
+	_dt.tv_sec = UtcYmdhmsToTime_T(year,mon-1,day,hour,min,(int)sec);
+	_dt.tv_nsec = (long)((sec - (long)sec)*1e9);
+}
+Date::Date(time_t time, double fracsec)
+: _secLeap(0)
+{
+	_dt.tv_sec = time;
+	_dt.tv_nsec = (long)(fracsec*1e9);
+}
+Date::Date( double secGps, int wkGps, int secLeap )
+: _secLeap(secLeap)
+{
+	time_t tsecGps = (time_t) secGps;
+	_dt.tv_sec = (time_t)(wkGps*SECONDS_IN_WEEK) + tsecGps + (time_t) _secLeap;
+	_dt.tv_sec += GPS0;
+	_dt.tv_nsec = (long)((secGps - tsecGps)*1e9);
+}
+
+Date::Date( const char* pszDate)
+{
+	int yr,mo,day,hr,min;
+	double sec;
+
+	//2012-10-12T00:06:12.314532
+	_snscanf( pszDate, strlen( pszDate), 
+		"%d-%d-%dT%d:%d:%lf",
+		&yr, &mo, &day, &hr, &min, &sec);
+
+	_dt.tv_sec = UtcYmdhmsToTime_T(yr,mo-1,day,hr,min,(int)sec);
+	_dt.tv_nsec = (long)((sec - (long)sec)*1e9);
+
+}
+
+const Date& Date::operator=( const Date& rhs)
+{
+	if( &rhs != this)
+	{
+		_dt = rhs._dt;
+		_secLeap = rhs._secLeap;
+	}
+	
+	return *this;
+}
 
 /**
  * Returns a string representation of the object.
  */
 String Date::toString( const String & sFormat )
 {
-	return _sdate;
+	char buff[64];
+	const struct tm* putc = TmPtr();
+	double dsec = putc->tm_sec + _dt.tv_nsec*1e-9;
+
+	//2012-10-12T00:06:12.314532Z
+	sprintf(buff, "%4d-%02d-%02dT%02d:%02d:%0.9lfZ",
+		putc->tm_year+1900, putc->tm_mon+1, putc->tm_mday,
+		putc->tm_hour, putc->tm_min, dsec);
+
+	return String( buff);
+}
+
+timespec Date::SinceGps0() const
+{ 
+	timespec gt = _dt; 
+	gt.tv_sec -= (GPS0 + _secLeap);
+	return gt;
+}
+
+/**
+ * Returns the GPS week and seconds 
+ */
+void Date::ToGpsWeekSec( int *pwkGps, double *psecGps) const
+{
+	timespec gt = _dt; 
+	gt.tv_sec -= (GPS0 + _secLeap);
+	*pwkGps = (int) (gt.tv_sec / SECONDS_IN_WEEK);
+	*psecGps = (double)(gt.tv_sec - (time_t)(SECONDS_IN_WEEK*(*pwkGps))); 
+	*psecGps += (gt.tv_nsec*1e-9);
 }
